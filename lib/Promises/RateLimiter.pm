faf371b5574bc203ed2b8b125f9832a8402dbaaa
Max Maischein <corion@corion.net>
2017-10-23 23:11:24 +0200

    Stuff

--- lib/Promises/RateLimiter.pm
+++ lib/Promises/RateLimiter.pm
@@ -7,11 +7,6 @@ use AnyEvent;
 use vars qw($VERSION);
 $VERSION = '0.01';
 
-Future::RateLimiter
-
- delay_class => 'AnyEvent::Future'
- delay_deferred => sub {...}
-
 =head1 NAME
 
 Promises::RateLimiter - rate limit paths through promises
