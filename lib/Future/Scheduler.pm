854c1e9f00da8e45661042455f3c4dc9346b0f38
Max Maischein <corion@corion.net>
2018-01-06 20:29:00 +0100

    Document $implementation and @loops

--- lib/Future/Scheduler.pm
+++ lib/Future/Scheduler.pm
@@ -19,7 +19,9 @@ a common API for the simple scheduling mechanisms available from the IO loops.
 
 =head2 Supported event loops
 
-Currently only L<AnyEvent>, L<Mojolicious> and L<IO::Async> are supported.
+Currently only L<AnyEvent>, L<Mojolicious> and L<IO::Async> are supported
+in addition to a trivial, blocking default backend if none of the above event
+loops can be detected.
 
 =cut
 
@@ -31,8 +33,7 @@ our @loops = (
     ['Mojo/IOLoop.pm'   => 'Future::Scheduler::Mojo' ],
     ['AnyEvent.pm'      => 'Future::Scheduler::AnyEvent'],
     ['AE.pm'            => 'Future::Scheduler::AnyEvent'],
-    # IO::Async support would be nice
-    
+
     # The fallback, will always catch due to loading Future itself
     ['Future.pm'        => 'Future::Scheduler::Future'],
 );
@@ -49,13 +50,13 @@ Creates a new instance of the scheduler abstraction.
 
 sub new($factoryclass, @args) {
     $implementation ||= $factoryclass->best_implementation();
-    
+
     # return a new instance
     $implementation->new(@args);
 }
 
 sub best_implementation( $class, @candidates ) {
-    
+
     if(! @candidates) {
         @candidates = @loops;
     };
@@ -69,7 +70,7 @@ sub best_implementation( $class, @candidates ) {
     } grep {
         $INC{$_->[0]}
     } @candidates;
-    
+
     # Check which one we can load:
     for my $impl (@applicable_implementations) {
         if( eval "require $impl; 1" ) {
@@ -89,13 +90,35 @@ sub best_implementation( $class, @candidates ) {
 Schedules a Future to be executed from the top level mainloop. This is usually
 equivalent to C<< ->sleep(0) >>.
 
+=head1 VARIABLES
+
+=head2 C<< @Future::Scheduler::loops >>
+
+This is the list of adapter implementations for the various backends. If you
+have another backend that is not yet supported by L<Future::Scheduler>, you
+can C<unshift> it to the list yourself as a workaround.
+
+The array holds the pairs of
+
+    [ "loaded/module.pm" => 'Backend::Implementation::Class' ],
+
+=head2 C<< $Future::Scheduler::implementation >>
+
+Holds the base class that provides the current implementation. This is
+initialized through
+
+  $Future::Scheduler::implementation = Future::Scheduler->best_implementation()
+
+If you really, really need to force a specific implementation rather than
+letting the module autodetect it, you can set it yourself.
+
 =head1 SEE ALSO
 
 L<Future>
 
 =head1 REPOSITORY
 
-The public repository of this module is 
+The public repository of this module is
 L<http://github.com/Corion/...>.
 
 =head1 SUPPORT
