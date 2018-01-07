96bfd8fa7844ecb5b299a1a05546ae377057fcde
Max Maischein <corion@corion.net>
2018-01-07 12:12:29 +0100

    Update repository information etc.

--- lib/Future/Scheduler/Functions.pm
+++ lib/Future/Scheduler/Functions.pm
@@ -50,8 +50,8 @@ L<Future::Scheduler>
 
 =head1 REPOSITORY
 
-The public repository of this module is 
-L<http://github.com/Corion/...>.
+The public repository of this module is
+L<http://github.com/Corion/Future-Scheduler>.
 
 =head1 SUPPORT
 
@@ -61,8 +61,8 @@ L<https://perlmonks.org/>.
 =head1 BUG TRACKER
 
 Please report bugs in this module via the RT CPAN bug queue at
-L<https://rt.cpan.org/Public/Dist/Display.html?Name=...>
-or via mail to L<future-http-Bugs@rt.cpan.org>.
+L<https://rt.cpan.org/Public/Dist/Display.html?Name=Future-Scheduler>
+or via mail to L<future-scheduler-Bugs@rt.cpan.org>.
 
 =head1 AUTHOR
 
--- lib/Future/Scheduler/IOAsync.pm
+++ lib/Future/Scheduler/IOAsync.pm
@@ -35,3 +35,33 @@ sub schedule( $self ) {
 }
 
 1;
+
+=head1 REPOSITORY
+
+The public repository of this module is
+L<http://github.com/Corion/Future-Scheduler>.
+
+=head1 SUPPORT
+
+The public support forum of this module is
+L<https://perlmonks.org/>.
+
+=head1 BUG TRACKER
+
+Please report bugs in this module via the RT CPAN bug queue at
+L<https://rt.cpan.org/Public/Dist/Display.html?Name=Future-Scheduler>
+or via mail to L<future-scheduler-Bugs@rt.cpan.org>.
+
+=head1 AUTHOR
+
+Max Maischein C<corion@cpan.org>
+
+=head1 COPYRIGHT (c)
+
+Copyright 2018 by Max Maischein C<corion@cpan.org>.
+
+=head1 LICENSE
+
+This module is released under the same terms as Perl itself.
+
+=cut
